a24fef1e25f8c0fc6d194c37452a0b94bff6cdcf
SANCHA1234 <agsancha.ag@gmail.com>
2025-04-22 10:48:50 +0200

    ad

---
+++ ejercicio1ex.js
@@ -0,0 +1,23 @@
+//*function invertirarray(mi_array)
+{
+    let array_invertido = []
+   for (let index=mi_array.lenght -1 ;index>=0;index--)
+{
+    array_invertido.push(mi_array[index]);
+    
+
+    } 
+}
+
+function menor2(a,b)
+{
+
+    if (a<b)
+    {
+        return a;
+        
+    }
+    else return b;
+}
+
+console.log (menor2(4,5));
