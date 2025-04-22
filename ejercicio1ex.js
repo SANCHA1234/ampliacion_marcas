c1287fe53ee6c2b8fcfc72e839f597d41a95aa50
SANCHA1234 <agsancha.ag@gmail.com>
2025-04-22 10:52:41 +0200

    qw

--- ejercicio1ex.js
+++ ejercicio1ex.js
@@ -20,4 +20,14 @@ function menor2(a,b)
     else return b;
 }
 
-console.log (menor2(4,5));
+//console.log (menor2(4,5));
+
+function menor4(a,b,c,d)
+{
+    let m1 = menor2(a,b);
+    let m2 = menor4(c,d);
+    return menor2(m1,m2);
+   
+}
+
+console.log(menor4(4,5,-6,20));
